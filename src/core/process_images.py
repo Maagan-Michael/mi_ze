ab9e29bd8a0c5c4fb83247745ead008757a6d3f4
KamiTzayig <Dyekozz@gmail.com>
2023-12-30 18:26:30 +0200

    replace face_detector with deepface package

---
+++ src/core/process_images.py
@@ -0,0 +1,30 @@
+from typing import List
+from deepface import DeepFace
+import numpy as np
+from core.cut_picture_from_box import cut_picture_from_box
+from core.data_models.face_data_model import FaceDataModel
+
+from core.data_models.image_data_model import ImageDataModel
+
+
+def process_images(images: List[ImageDataModel]):
+    images_with_faces = []
+    
+    for image in images:
+        print(image.image_id)
+        image_path = image.get_image_path()
+        face_dicts =  DeepFace.represent(img_path=image_path, model_name="Facenet512",detector_backend="retinaface",enforce_detection=False)
+        for i,d in enumerate(face_dicts):
+            face_image_file_name = f"{image.get_image_id()}_face_{i}.jpg"
+            encoding = np.array(d["embedding"])
+            box =np.array( [d["facial_area"]["x"],d["facial_area"]["y"],d["facial_area"]["w"],d["facial_area"]["h"]])
+            face_image_path =  cut_picture_from_box(picture_path=image_path,box= box, output_dir="images/extracted_faces",file_name=face_image_file_name)
+
+            face = FaceDataModel(imageId=image.get_image_id(),faceId= f"{image.get_image_id()}_face_{i}", faceImagePath= face_image_path, personId= None, box= box,face_encoding=encoding)
+            image.add_face(face)
+        images_with_faces.append(image)
+
+    return images_with_faces
+    
+
+
--- src/main.py
+++ src/main.py
@@ -5,30 +5,25 @@ import cv2
 from core.data_models.face_data_model import FaceDataModel
 from core.data_models.image_data_model import ImageDataModel
 from core.data_models.person_data_model import PersonDataModel
+from core.process_images import process_images
 
-from core.yolov8_face import YOLOv8_face
 import csv
 from images_from_csv import images_from_csv
 from images_to_csv import images_to_csv
 
-from process_images import process_images
 
 images = [ImageDataModel( image_path=r'images\test3.jpg',image_id="test3"),ImageDataModel( image_path=r'images\test_image.png',image_id="test"),ImageDataModel( image_path=r'images\test4.jpg',image_id="test4"),ImageDataModel( image_path=r'images\test_image2.jpeg',image_id="test2"), ImageDataModel( image_path=r'images\test5.jpg',image_id="test5"), ImageDataModel( image_path=r'images\test6.jpg',image_id="test6"), ImageDataModel( image_path=r'images\test7.jpg',image_id="test7"), ImageDataModel( image_path=r'images\test8.jpg',image_id="test8"), ImageDataModel( image_path=r'images\test9.jpg',image_id="test9"), ImageDataModel( image_path=r'images\test10.jpg',image_id="test10"), ImageDataModel( image_path=r'images\test11.jpg',image_id="test11"), ImageDataModel( image_path=r'images\test12.jpg',image_id="test12"), ImageDataModel( image_path=r'images\test13.jpg',image_id="test13"), ImageDataModel( image_path=r'images\test14.jpg',image_id="test14"), ImageDataModel( image_path=r'images\test15.jpg',image_id="test15"), ImageDataModel( image_path=r'images\test16.jpg',image_id="test16"), ImageDataModel( image_path=r'images\test17.jpg',image_id="test17"), ImageDataModel( image_path=r'images\test18.jpg',image_id="test18"), ImageDataModel( image_path=r'images\test19.jpg',image_id="test19"), ImageDataModel( image_path=r'images\test20.jpg',image_id="test20"), ImageDataModel( image_path=r'images\test21.jpg',image_id="test21"), ImageDataModel( image_path=r'images\test22.jpg',image_id="test22"), ImageDataModel( image_path=r'images\test23.jpg',image_id="test23"), ]
-# images = [ImageDataModel( image_path=r'images\test3.jpg',image_id="test3"),ImageDataModel( image_path=r'images\test_image.png',image_id="test")]
 
 
 
-# Initialize YOLOv8_face object detector
-YOLOv8_face_detector = YOLOv8_face(conf_thres=0.4, iou_thres=0.5)
-known_processed_images = process_images(images=images, neural_net_model=YOLOv8_face_detector)
 
-images_to_csv(known_processed_images)
-# asdf
+# processed_images = process_images(images=images)
+# images_to_csv(processed_images)
 processed_images = images_from_csv()
-kami_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_3"] 
-omri_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_1" or face.faceId == "test4_face_7" ]
-yaron_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_2" or  face.faceId == "test17_face_2"]
-amit_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_7"]
+kami_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_5"] 
+omri_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_4"  ]
+yaron_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_3" ]
+amit_faces = [face for image in processed_images for face in image.faces if face.faceId == "test3_face_2" ]
 people = [ PersonDataModel(name='Kami Tazyig',personId="2",verified_faces=kami_faces), PersonDataModel(name='Omri Berchman',personId="1",verified_faces=omri_faces),PersonDataModel(name='Yaron Shaharabani',personId="3",verified_faces=yaron_faces), PersonDataModel(name='Amit Inbar',personId="4",verified_faces=amit_faces)]
 
 
@@ -46,6 +41,8 @@ people = [ PersonDataModel(name='Kami Tazyig',personId="2",verified_faces=kami_f
 
 
 import face_recognition
+from deepface.commons import functions, realtime, distance as dst
+
 known_faces: List[FaceDataModel] = [face for image in processed_images for face in image.faces]
 people_faces = [(person,face) for person in people for face in person.verified_faces]
 # known_faces = [face.get_face_encoding() for face in [image.faces for image in processed_images]]
@@ -61,11 +58,16 @@ for image in processed_images:
         if(len(face.face_encoding) <1):
             continue
         face_encoding = face.get_face_encoding()
-        results = face_recognition.face_distance(face_encodings=[p_f[1].get_face_encoding() for p_f in people_faces],face_to_compare=face_encoding)
+        results = []
+        for known_face_encoding in [p_f[1].get_face_encoding() for p_f in people_faces]:
+            distance = dst.findCosineDistance(known_face_encoding, face_encoding)
+            results.append(distance)
+
+        # results = face_recognition.face_distance(face_encodings=[p_f[1].get_face_encoding() for p_f in people_faces],face_to_compare=face_encoding)
         most_similar_face = min(results)
 
         for i in range(len(results)):
-            if results[i] <= most_similar_face and results[i] <= 0.55:
+            if results[i] <= most_similar_face and results[i] <= 0.6:
                 verified_person_face = people_faces[i][1]
                 person =  people_faces[i][0]
                 if person.personId not in d or d[person.personId][2] > results[i]:
@@ -88,7 +90,7 @@ for image in processed_images:
 
         # Add face ID
         cv2.putText(srcimg, person.name , (int(x-50), int(y - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 3)
-        cv2.putText(srcimg,str(match_distance), (int(x), int(y - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 0), 2)
+        cv2.putText(srcimg,str(round(match_distance,3)), (int(x), int(y - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 0), 2)
 
 
     cv2.imshow("image", srcimg)
